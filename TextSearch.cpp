aa894a85e2ec7374fb3656eca270f312adce0607
Maximilian Keil <maxikeil.mk@gmail.com>
2016-07-27 18:58:58 +0200

    filereader

--- TextSearch.cpp
+++ TextSearch.cpp
@@ -1,9 +1,39 @@
 #include "TextSearch.h"
+#include <fstream>
 #include <iostream>
 
 using namespace std;
 
+void readFile(const string inputFile) {
+    ifstream ifStream;
+    string s;
+    string result;
+    ifStream.open(inputFile);
+    if (ifStream.is_open()) {
+        cout << "reading ... " << endl;
+
+        while (!ifStream.eof()) {
+            getline(ifStream, s);
+            result.append("\n");
+            result.append(s);
+        }
+        ifStream.close();
+
+        cout << result << endl;
+        cout << "file read" << endl;
+    } else {
+        cout << "File not found" << endl;
+    }
+}
+
 int main() {
-    cout << "Hello World!";
+    string input;
+
+    cout << "Hallo Dudeberger, gib mal Textdatei !" << endl;
+    cin >> input;
+    cout << input << endl;
+
+    readFile(input);
+
     return 0;
 }
--- TextSearch.h
+++ TextSearch.h
@@ -3,13 +3,19 @@
 
 #include <string>
 
+using namespace std;
+
 class TextSearch {
 
 private:
-    
+
+    string text;
+    void readFile(const string);
 
 public:
 
+    void setText(const string text) {this->text = text;};
+    string getText() {return text;};
 };
 
 #endif //BBTREE_BBTREE_H
