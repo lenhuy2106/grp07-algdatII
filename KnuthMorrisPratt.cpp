63af23c8b7647088ac1eff091f840122b841e9d5
Long Yan <long.myan@gmail.com>
2016-07-27 19:02:28 +0200

    KMP

---
+++ KnuthMorrisPratt.cpp
@@ -0,0 +1,14 @@
+//
+// Created by lenovo on 7/27/2016.
+//
+#include <iostream>
+#include "KnuthMorrisPratt.h"
+
+using namespace std;
+
+int main() {
+
+    cout << "hallo world";
+    return 0;
+
+}
---
+++ KnuthMorrisPratt.h
@@ -0,0 +1,16 @@
+//
+// Created by lenovo on 7/27/2016.
+//
+
+#ifndef GRP07_ALGDATII_KNUTHMORRISPRATT_H
+#define GRP07_ALGDATII_KNUTHMORRISPRATT_H
+
+using namespace std;
+
+class KnuthMorrisPratt{
+
+    
+
+};
+
+#endif //GRP07_ALGDATII_KNUTHMORRISPRATT_H
