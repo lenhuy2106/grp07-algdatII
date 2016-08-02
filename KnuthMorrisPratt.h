9012518e75ae38e505ad2cb626309aa490ce7db0
Maximilian Keil <maxikeil.mk@gmail.com>
2016-08-02 12:10:03 +0200

    Doxygen erweitert

--- KnuthMorrisPratt.h
+++ KnuthMorrisPratt.h
@@ -1,30 +1,73 @@
-//
-// Created by lenovo on 7/27/2016.
-//
-
 #ifndef GRP07_ALGDATII_KNUTHMORRISPRATT_H
 #define GRP07_ALGDATII_KNUTHMORRISPRATT_H
+
 #include <string>
 
 using namespace std;
 
+/**
+ * Klasse repräsentiert den Knuth-Morris-Pratt Suchalgorithmus für dynamische Texte.
+ * Genauere beschreibung siehe Textsearch.h.
+ */
 class KnuthMorrisPratt{
 
     private:
-        
+
+        /**
+         *
+         */
         string pattern;
+
+        /**
+         *
+         */
         string text;
+
+        /**
+         *
+         */
         int* prefixTable;
+
+        /**
+         *
+         */
         string htmlOutput;
+
+        /**
+         *
+         */
         int counter;
 
+        /**
+         *
+         */
         void generatePrefixTable();
+
+        /**
+         *
+         * @param subText
+         * @return
+         */
         int doKMPAlgorithmn(string subText);
+
+        /**
+         *
+         */
         void writeHtmlFile();
+
+        /**
+         *
+         */
         void run();
 
     public:
 
+        /**
+         * 
+         * @param t
+         * @param p
+         * @return
+         */
         KnuthMorrisPratt(string t, string p) : text(t), pattern(p){
             run();
         }
